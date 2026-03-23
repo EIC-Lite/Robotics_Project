346f9459917348c3dda794767077b3de8d40394c
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2026-03-23 22:17:10 +0700

    Create robot.py

---
+++ robot.py
@@ -0,0 +1,13 @@
+import socket, time
+from config import *
+
+
+def robot_connect():
+    # Connect to robot
+    global r
+    r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    r.connect((ip_robot, port_rtde))
+    print('Connected to robot')
+
+    return r
+
