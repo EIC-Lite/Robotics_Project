64d3ac6d25e1d3f46390a6a72f58d4edc40683e0
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2026-03-23 22:17:12 +0700

    Create gripper.py

---
+++ gripper.py
@@ -0,0 +1,41 @@
+import socket, time
+from config import *
+
+
+def gripper_connect():
+    # Connect to gripper
+    global g
+    g = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    g.connect((ip_robot, port_g))
+    g.send(b'GET ACT\n')
+
+    # Activate the gripper
+    g_recv = str(g.recv(10), 'UTF-8')
+    print('GET ACT = ' + g_recv)
+    g.send(b'GET POS\n')
+    g_recv = str(g.recv(10), 'UTF-8')
+    if g_recv:
+        g.send(b'SET ACT 1\n')
+        g_recv = str(g.recv(255), 'UTF-8')
+        time.sleep(3)
+        g.send(b'SET GTO 1\n')
+        g.send(b'SET SPE 255\n')
+        g.send(b'SET FOR 200\n')
+    print('Gripper ready')
+
+    return g
+
+
+def gripper_open():
+    # Open the gripper
+    g.send(b'SET POS 0\n')
+    g.recv(255)
+    time.sleep(1)
+
+
+def gripper_close():
+    # Close the gripper
+    g.send(b'SET POS 200\n')
+    g.recv(255)
+    time.sleep(1)
+
