0641a09f5f7ebe35e9e5d252a0bc28cfb67fc559
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2026-04-07 10:04:19 +0700

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,4 +1,4 @@
-import socket, time
+import socket, time, math
 from config import *
 
 
@@ -10,4 +10,21 @@ def connect():
     print('Connected to robot')
 
     return r
+
+
+def rotate_rz(RZ):
+    # Rotate Rz
+    rz = math.radians(RZ)
+    cmd = (
+        f'movej(['
+        f'get_actual_joint_positions()[0], '
+        f'get_actual_joint_positions()[1], '
+        f'get_actual_joint_positions()[2], '
+        f'get_actual_joint_positions()[3], '
+        f'get_actual_joint_positions()[4], '
+        f'get_actual_joint_positions()[5] + {rz:.6f}'
+        f'], 1.2, 0.4, 0, 0)\n'
+    )    
+    r.send(cmd.encode())    
+    time.sleep(1)
 
